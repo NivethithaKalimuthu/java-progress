3d098d36eb0e83df9e551261fe5dcefae47ee552
NivethithaKalimuthu <nivethithakalimuthu2258@gmail.com>
2026-02-15 12:14:01 +0530

    just add

---
+++ adding.java
@@ -0,0 +1,10 @@
+public class adding {
+    public static void main(String[] args) {
+        int a=20;
+        int b=19;
+        int c=30;
+        int d=a+b+c;
+        System.out.println(d);
+    }
+    
+}
