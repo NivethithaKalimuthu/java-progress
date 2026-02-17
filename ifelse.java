cdce9c471517e4252105ac87f189518d16602d7c
NivethithaKalimuthu <nivethithakalimuthu2258@gmail.com>
2026-02-17 22:06:47 +0530

    if else concept

---
+++ ifelse.java
@@ -0,0 +1,15 @@
+public class ifelse {
+    public static void main(String[] args) {
+        boolean rain = true;
+        if (rain)
+        {
+            System.out.println("take umbrella");
+        }
+        else
+        {
+            System.out.println("enjoy sunlight");
+        }
+    
+    }
+    
+}
