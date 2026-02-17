225cbbadbd7228b9b229024dfd0e3b62fb8d3415
NivethithaKalimuthu <nivethithakalimuthu2258@gmail.com>
2026-02-17 22:09:02 +0530

    comparing two values

---
+++ comparison2.java
@@ -0,0 +1,19 @@
+public class comparison2 {
+    public static void main(String[] args) {
+        int num1=40;
+        int num2=60;
+        if(num1==num2)
+        {
+            System.out.println(true);
+        }
+        else
+        {
+            System.out.println(false);
+        }
+           
+
+    
+        
+    }
+    
+}
