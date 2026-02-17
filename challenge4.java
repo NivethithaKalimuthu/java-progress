6eb2eb4e5d4a39cd2a86a2beb6070adef6e1e48f
NivethithaKalimuthu <nivethithakalimuthu2258@gmail.com>
2026-02-17 22:09:48 +0530

    daily challenge

---
+++ challenge4.java
@@ -0,0 +1,19 @@
+import java.lang.System;
+import java.util.Scanner;
+public class challenge4 {
+    public static void main(String[] args) {
+        Scanner nivi=new Scanner(System.in);
+        String rcb=nivi.nextLine();
+        if(rcb.equals("win"))
+        {
+            System.out.println("ee sala cup namdhey");
+        }
+        else{
+            System.out.println("cup ella");
+
+        }
+        nivi.close();
+        
+    }
+    
+}
