cb3e278304d1525daf6619daf57bd412717c0964
NivethithaKalimuthu <nivethithakalimuthu2258@gmail.com>
2026-02-18 21:36:53 +0530

    nestedif concept

---
+++ nestedif.java
@@ -0,0 +1,11 @@
+public class nestedif {
+    public static void main(String[] args) {
+        int age=20;
+        boolean isindian=true;
+        if(age>18)
+        {
+            if(isindian)
+            System.out.println("you are eligible to vote");
+        }
+    }
+}
