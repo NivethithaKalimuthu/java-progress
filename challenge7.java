70c69b74b1c48e53e123170c39c3850486aa1ece
NivethithaKalimuthu <nivethithakalimuthu2258@gmail.com>
2026-02-18 21:11:30 +0530

    daily challenge

---
+++ challenge7.java
@@ -0,0 +1,15 @@
+public class challenge7 {
+    public static void main(String[] args) {
+        boolean cricket=false;
+        boolean football=false;
+        if(cricket||football)
+        {
+            System.out.println("enjoy");
+        }
+        else{
+            System.out.println("sad");
+        }
+    
+    }
+    
+}
