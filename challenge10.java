94c7b1a97a6e512709b12bb5ada6e1b1981d372a
NivethithaKalimuthu <nivethithakalimuthu2258@gmail.com>
2026-02-18 21:48:32 +0530

    daily code

---
+++ challenge10.java
@@ -0,0 +1,18 @@
+public class challenge10 {
+    public static void main(String[] args) {
+        int score=80;
+        if(score<50)
+        {
+            System.out.println("you need to improve");
+        }
+        else if(score>=50 && score<=70)
+        {
+            System.out.println("good job");
+        }
+        else if(score>70)
+        {
+            System.out.println("excellent performance");
+ 
+        }
+    }
+}
