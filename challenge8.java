71381091d3ec732afb9ee63560f92b932d3e2349
NivethithaKalimuthu <nivethithakalimuthu2258@gmail.com>
2026-02-18 21:12:09 +0530

    daily code

---
+++ challenge8.java
@@ -0,0 +1,15 @@
+public class challenge8 {
+    public static void main(String[] args) {
+        int num =15
+        ;
+        if(num%3==0 && num%5==0)
+        {
+            System.out.println("yes");
+        }
+        else
+        {
+            System.out.println("not");
+        }
+
+    }
+}
