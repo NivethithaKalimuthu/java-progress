94d060036b3d3849e6d4f28a5f82ef9e1ea3b5ae
NivethithaKalimuthu <nivethithakalimuthu2258@gmail.com>
2026-02-18 21:36:22 +0530

    else concept

---
+++ elsenested.java
@@ -0,0 +1,20 @@
+public class elsenested{
+    public static void main(String[] args) {
+        int score=30;
+        if(score>35 && score<60)
+        {
+            System.out.println("good");
+        
+        }
+        else if(score>60 && score<90)
+        {
+            System.out.println("very good");
+
+        }
+        else if(score>90);{
+            System.out.println("very nice");
+        }
+        
+
+    }
+}
