2011cc389ab578ca039208545142168534efb501
NivethithaKalimuthu <nivethithakalimuthu2258@gmail.com>
2026-02-21 12:40:29 +0530

    day 2

---
+++ variables2.java
@@ -0,0 +1,12 @@
+public class variables2{
+
+    public static void main(String[] args) {
+        int container=67;
+        String container2= "nivi";
+        System.out.println(container);
+        System.out.println(container2);
+
+
+    }
+}    
+
