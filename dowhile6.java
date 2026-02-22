9bff030f6cd760a1ac3dc8b9f479deea8a513837
NivethithaKalimuthu <nivethithakalimuthu2258@gmail.com>
2026-02-22 19:37:58 +0530

    dowhile concept

---
+++ dowhile6.java
@@ -0,0 +1,9 @@
+public class dowhile6 {
+    public static void main(String[] args) {
+        int count=1;
+        do{
+            System.out.println("nivi");
+            count=count+1;
+        }while(count<=1);
+    }
+}
