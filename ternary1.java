1d1f0a3c9cffd720261040ad4a57ac17700cc40a
NivethithaKalimuthu <nivethithakalimuthu2258@gmail.com>
2026-02-22 12:27:09 +0530

    ternary operator

---
+++ ternary1.java
@@ -0,0 +1,8 @@
+public class ternary1 {
+    public static void main(String[] args) {
+        boolean rain=true;
+        String result=rain?"take umbrella":"enjoy sun";
+        System.out.println(result);
+
+    }
+}
