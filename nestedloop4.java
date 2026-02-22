dd3406d2554a9d80eecfa8eb6632b24241da0dbf
NivethithaKalimuthu <nivethithakalimuthu2258@gmail.com>
2026-02-22 19:36:49 +0530

    nestedloop concept

---
+++ nestedloop4.java
@@ -0,0 +1,11 @@
+public class nestedloop4 {
+    public static void main(String[] args) {
+        for(int i=1;i<=3;i=i+1){
+            for(int j=1;j<=2;j=j+1){//it runs based on the i
+                System.out.println("nivi");
+            }
+            
+
+        }
+    }
+}
