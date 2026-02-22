7a6b4da346c92ca6a233ed88c49341407d602750
NivethithaKalimuthu <nivethithakalimuthu2258@gmail.com>
2026-02-22 19:37:26 +0530

    while loop

---
+++ whileloop5.java
@@ -0,0 +1,11 @@
+ public class whileloop5{
+    public static void main(String[] args) {
+        int i=1;
+        while(i<=3){
+            System.out.println("nivi");
+            i++;
+
+        }
+    }
+    
+}
