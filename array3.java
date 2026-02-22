3decee41d5b1afd3298dd00b97fc7f5bdfd2fdf7
NivethithaKalimuthu <nivethithakalimuthu2258@gmail.com>
2026-02-22 12:28:43 +0530

    array concept

---
+++ array3.java
@@ -0,0 +1,8 @@
+public class array3 {
+    public static void main(String[] args) {
+        String[] playlist=new String[3];// String[] playlist={"song1","song2","song3"};
+        playlist[1]="song1";
+        System.out.println(playlist[1]);
+
+    }
+}
