f103c7661e6cb873927e96ddbb260efa21ddc33e
NivethithaKalimuthu <nivethithakalimuthu2258@gmail.com>
2026-02-22 19:38:50 +0530

    variable and object

---
+++ variablesobject7.java
@@ -0,0 +1,15 @@
+public class variablesobject7 {
+    int tea=20;
+    int coffee=30;
+    public static void main(String[] args) {
+        variablesobject7 server1=new variablesobject7();//obj is created for class
+        {
+            System.out.println(server1.tea);
+        }
+        variablesobject7 server2=new variablesobject7();
+        {
+            System.out.println(server2.coffee);
+        }
+    }
+    
+}
